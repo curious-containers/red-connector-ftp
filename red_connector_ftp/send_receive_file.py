69eaf08755255eb47b0e678d950729c24d996c8c
Bluemi <bruno.schilling@protonmail.ch>
2019-11-04 12:56:06 +0100

    typos

--- red_connector_ftp/send_receive_file.py
+++ red_connector_ftp/send_receive_file.py
@@ -1,4 +1,4 @@
-RECEIVE_FILE_DESCRIPTION = 'Receive input file from SSH server.'
+RECEIVE_FILE_DESCRIPTION = 'Receive input file from FTP server.'
 RECEIVE_FILE_VALIDATE_DESCRIPTION = 'Validate access data for receive-file.'
 
 

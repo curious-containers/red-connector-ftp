07bec9bc0fc98cc617ffd215ef56e491bb17abe5
Bluemi <bruno.schilling@protonmail.ch>
2019-11-04 12:54:32 +0100

    init project structure

---
+++ red_connector_ftp/__main__.py
@@ -0,0 +1,5 @@
+from red_connector_ftp.main import main
+
+
+if __name__ == 'main':
+    main()
---
+++ red_connector_ftp/send_receive_file.py
@@ -0,0 +1,10 @@
+RECEIVE_FILE_DESCRIPTION = 'Receive input file from SSH server.'
+RECEIVE_FILE_VALIDATE_DESCRIPTION = 'Validate access data for receive-file.'
+
+
+def receive_file():
+    pass
+
+
+def receive_file_validate():
+    pass
